ba7f3affb68837bd0ac96f9d5528d72009404bca
Smigal Target <qt.target@gmail.com>
2014-10-09 15:42:30 +0100

    louvriture de grid et img et shapefile

--- TP_Khalid_1/Form1.vb
+++ TP_Khalid_1/Form1.vb
@@ -12,16 +12,39 @@
         Dim dlg As New OpenFileDialog
         dlg.Filter = "All Files (*.*)|*.*|" & sf.CdlgFilter & "|" & grd.CdlgFilter & " | " & img.CdlgFilter
         If dlg.ShowDialog() = Windows.Forms.DialogResult.OK Then
-            ' on affecter l'extention du fichier ouvrir en  miniscule et l'afficher
+            ' on affecter l'extention du fichier ouvrir en  miniscule
             Dim extention As String
             extention = IO.Path.GetExtension(dlg.FileName).ToLower()
+            ' si lextension du fichier ouvert est dans les extentions de shapefile 
             If sf.CdlgFilter.ToLower().Contains(extention) Then
+                'on ouvre le fichier
+                ' et on lajoute dans la carte 
                 sf.Open(dlg.FileName)
                 carte.AddLayer(sf, True)
+                carte.ZoomToMaxExtents()
+                ' l objectif est eteind
+                Return
+                ' traitement dune gride 
+            ElseIf grd.CdlgFilter.ToLower().Contains(extention) Then
+                ' le .tiff est un geotiff donc on le traite comme tiff
+
+                If dlg.FileName.ToLower().EndsWith("tiff") Then
+                    If Not carte.IsTIFFGrid(dlg.FileName) Then
+                        img.Open(dlg.FileName)
+                        carte.AddLayer(img, True)
+                        'objectif atteind
+                        Return
+                    End If
+                End If
+                grd.Open(dlg.FileName)
+                carte.AddLayer(grd, True)
+
 
 
 
             End If
+
+
 
 
 
